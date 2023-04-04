4c425be732ec3fe3bc5d49defaf91f12f00576f5
Shouvik Mukhopadhyay <mukhopadhyay.shouvik24@gmail.com>
2023-04-04 20:26:22 +0530

    Initial Commit : Created Site Layout and Pagination Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full border text-center shadow-md py-4 uppercase fixed top-0 bg-white">
+      <header className="text-3xl font-bold">Shouvik Mukhopadhyay Blogs</header>
+    </div>
+  );
+};
+
+export default Header;
